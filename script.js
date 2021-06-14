b12efeb93146b707e2222f11fe2e75518eeff3dc
rosy-web <rosalba.diaz@informador.com.mx>
2021-06-14 01:46:38 +0000

    Changes for Final Project

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
-}
+    var prin = parseInt(document.getElementById("principal").value);
+    if(isNaN(prin)||prin<=0)
+    { 
+        document.getElementById("alert").innerHTML = "Enter a positive number to calculate"; 
+        document.getElementById("wpr").className = "maindiv error";
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+        var rate = document.getElementById("rate").value;
+        var year = document.getElementById("years").value;
+        var fyea = new Date().getFullYear()+parseInt(year);
+
+        var amount = prin * year * rate /100;
+        var fmt = new Intl.NumberFormat();
         
+        document.getElementById("result").innerHTML = "<p>If you deposit <b>$"+fmt.format(prin)+"</b>,<br>at an interest rate of <b>"+rate+"%</b>.<br>You will receive an amount of <b>$"+fmt.format(amount)+"</b>,<br>in the year <b>"+fyea+"</b></p>"
+        document.getElementById("wpr").className = "maindiv result";
+    }
+}
+function reset()
+{
+    document.getElementById("wpr").className = "maindiv";
+    document.getElementById("principal").value = "";
+    document.getElementById("rate").value = 10.25;
+    document.getElementById("rate_val").innerText="10.25%";
+    document.getElementById("years").value = 1;
+    document.getElementById("principal").focus();
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
